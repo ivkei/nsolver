a5c70e61ca67b4bed77fb9e6def8be94e3556b33
debugkei <ivankei0009@gmail.com>
2026-06-20 15:59:33 -0700

    Removed todos

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 int main(){
 
   //TODO: SolveBT only works with signs placed ones
-  //TODO: Also it needs major refactoring!
   std::cout << "Solution to 24, {1, 5, 5 ,5}: " << nsolver::BruteForce(24, {1,5,5,5}) << std::endl;
   std::cout << "Solution to 24, {1, 3, 4 ,6}: " << nsolver::BruteForce(24, {1,3,4,6}) << std::endl;
 
